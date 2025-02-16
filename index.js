a96a3c242e8bcf0a37a6cd20d6bfc4ad05d7dabd
wellborgmann2 <wellborgmann2@gmail.com>
2025-02-15 23:52:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import {criarPix} from './src/mercadopago.js';
-import {getUsers} from './src/banco.js';
+import {verifyGoogleToken} from './src/google.js';
+import {
+userInfo,
+registerAccount
+} from './src/banco.js';
 const app = express();
 const PORT = 3000;
 
@@ -19,6 +23,44 @@ app.get("/", async (req, res) => {
     console.error(error);
   }
 });
+
+
+
+app.get("/iptv", async (req, res) => {
+res.send("iptv");
+  });
+  
+
+
+  app.post("/google", async (req, res) => {
+  const token = req.body.credential;
+
+  try {
+    const payload = await verifyGoogleToken(token);
+    const userEmail = payload.email;
+    req.session.userEmail = userEmail;
+
+    try {
+      const user = await userInfo(userEmail);
+
+      if (user.length == 0) {
+        await registerAccount(payload);
+      }
+      
+      res.redirect("/iptv");
+    } catch (error) {
+      console.error("Erro ao verificar/extrair informações do usuário:", error);
+      res.redirect("/");
+    }
+  } catch (error) {
+    console.error("Erro ao verificar o token de ID do Google:", error);
+    res.status(401).json({
+      error: "Erro ao verificar o token de ID do Google",
+      motivo: error.message,
+    });
+  }
+});
+
 
 // Iniciando o servidor Express
 app.listen(PORT, () => {
